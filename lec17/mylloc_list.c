e6bbb15689db4af0cbc0a4f9e43bbeeefa39df7e
jamccarty <jmccarty@DESKTOP-9KGPCJM.localdomain>
2022-03-29 13:21:15 -0400

    committing old code

--- lec17/mylloc_list.c
+++ lec17/mylloc_list.c
@@ -12,11 +12,33 @@ void *malloc (size_t size) {
   if (size == 0){
     return NULL;
   }
+
+  //Step 1: check free list for memory
+  //if found, remove it from free list, and return 
+  
+  //Step 2: only allocated if no memory found
   void *memory = sbrk(size);
   if (memory == (void *) -1) {
     return NULL;
   } else {
-    return memory;
+    struct chunk *ptr = flist;
+    chunk *prev = NULL;
+
+    while(ptr != NULL){
+      if(ptr -> size >= size){
+        if(prev != NULL){
+          return (void *)(ptr + 1);
+        }else{
+          flist = ptr -> next;
+        }
+        return (void *)(ptr + 1);
+      }else{
+        prev = ptr;
+        ptr = ptr -> next;
+      }
+    }
+    prev -> next = ptr;
+    return (void *memory)(ptr + 1);
   }
 }
 
