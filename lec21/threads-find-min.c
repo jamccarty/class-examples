dbc6bf6f5f1b146811504b14c17f193be771740b
jamccarty <jmccarty@DESKTOP-9KGPCJM.localdomain>
2022-04-14 13:15:14 -0400

    Committing to avoid merge error later

--- lec21/threads-find-min.c
+++ lec21/threads-find-min.c
@@ -2,6 +2,19 @@
 #include <time.h>
 #include <stdlib.h>
 
+struct thread_data{
+  int *array;
+  int start_index;
+  int end_index;
+  int min;
+};
+
+
+void* find_min(void* userdata){
+  struct thread_data *data = (struct thread_data*)user_data;
+  return (void*)NULL;
+}
+
 int main(int argc, char *argv[]) {
   srand(time(0));
 
@@ -16,7 +29,8 @@ int main(int argc, char *argv[]) {
   size = strtol(argv[1], NULL, 10);
 
   values = malloc(sizeof(int) * size);
-  int min = RAND_MAX;
+  int min = RAND_MAX; //part of RAND api, largest number rand can return
+
   for (int i = 0; i < size; i++) {
     values[i] = rand() % 10000;
     min = values[i] < min? values[i] : min;
@@ -24,6 +38,28 @@ int main(int argc, char *argv[]) {
   }
 
   printf("The min value is %d\n", min);
+
+  pthread_t threads[4];
+  struct pthread_data data[4];
+  int subsize = size/4; //assume multiple of 4
+  for(int i =0; i < 4; i++){
+    data[i].array = values;
+    data[i].start_index = subsize*i;
+    data[i].end_index = subsize*(i+1);
+    pthread_create(&threads[i], NULL, find_min, &data[i]);
+  }//if want ret value from find_min, need to malloc it
+  //instead, put return value in thread_data struct
+  //to avoid having to malloc additional memory
+
+  for(int i = 0; i < 4; i++){
+    pthread_join(threads[i], NULL);
+  }
+
+  int gmin = RAND_MAX;
+  for(int i = 0; i < 4; i++){
+    gmin = data[i].min < gmin? data[i].min : gmin;
+  }
+
   free(values);
   
 }
